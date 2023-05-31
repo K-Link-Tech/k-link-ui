80cab677046d6abcf56da8d70f2210e8021c2a7c
Phyo Yarzar <phyoyazar592@gmail.com>
2023-05-31 16:33:30 +0630

    fix date range picker error (one more day UI)

--- src/k-link-dates/src/hooks/use-dates-state/use-dates-state.ts
+++ src/k-link-dates/src/hooks/use-dates-state/use-dates-state.ts
@@ -6,8 +6,8 @@ import { useUncontrolledDates } from '../use-uncontrolled-dates/use-uncontrolled
 function isInRange(date: Date, range: [Date, Date]) {
   const _range = [...range].sort((a, b) => a.getTime() - b.getTime());
   return (
-    dayjs(date).subtract(1, 'day').isBefore(_range[1]) &&
-    dayjs(date).add(1, 'day').isAfter(_range[0])
+    dayjs(_range[0]).startOf('day').subtract(1, 'ms').isBefore(date) &&
+    dayjs(_range[1]).endOf('day').add(1, 'ms').isAfter(date)
   );
 }
 
